6f42d865de0c290938efadf80cc3b8b46e186da4
bobah165 <43619200+bobah165@users.noreply.github.com>
2019-01-30 17:33:14 +0300

    Create HelloWorld.java
    First Commite

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String args[] ) throws Exception {
+System.out.println("Hello World");
+}
